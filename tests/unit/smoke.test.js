1dd4f170f91fa3a17f45f4d838544ba5eaa43232
Tuo <clarkhtse@gmail.com>
2017-08-03 15:19:43 +0800

    added smoke test

---
+++ tests/unit/smoke.test.js
@@ -0,0 +1,16 @@
+var supertest = require('supertest')(require('../../app'));
+var expect = require('chai').expect;
+var cheerio = require('cheerio');
+
+describe('loading express', function() {
+
+    it('responds to /', function test(done){
+        supertest.get('/').expect(200).end(function(err, res) {
+            var $ = cheerio.load(res.text);
+            expect($('h1').text()).to.equal('Express');
+            expect($('p').text()).to.equal('Welcome to Express');
+            if (err) throw err;
+            done();
+        });
+    })
+});
